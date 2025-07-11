9cb72604a403f4db59d183e5cffc6f37244658c8
Abhishek Gajera <abhishek@verityplatforms.com>
2025-07-11 12:10:40 +0530

    changes user

--- controller/api/userController.js
+++ controller/api/userController.js
@@ -1,4 +1,4 @@
-const { UserModel } = require('../../models/index');
+const  UserModel = require('../../models/userModel');
 const Joi = require('joi');
 const { hashPassword, generateJWTToken, comparePassword } = require('../../services/passwordUtils');
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,33 +1,15 @@
-module.exports = (sequelize, Sequelize, DataTypes) => {
-    const User = sequelize.define("user", {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-            allowNull: false,
-        },
-        userName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        jwtToken: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        status: {
-            type: DataTypes.ENUM("Active", "InActive"),
-            defaultValue: "Active",
-        }
-    });
-    return User;
-};
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
+const UserSchema = new mongoose.Schema({
+    id: { type: String, default: uuidv4, unique: true },
+    userName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    jwtToken: { type: String, default: null },
+    status: { type: String, enum: ["Active", "InActive"], default: "Active" },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', UserSchema);
